e971685781a7101a6c1f4b3b96889046df11d837
Larry Kyrala <larry.kyrala@gmail.com>
2013-02-10 20:43:17 -0500

    added tests for complete coverage

--- test/unit/data_warehouse_test.rb
+++ test/unit/data_warehouse_test.rb
@@ -5,12 +5,29 @@ class DataWarehouseTest < MiniTest::Unit::TestCase
   def setup
   end
 
+  def teardown
+    FileUtils.rm("foo.db", force: true)
+  end
+
   def test_cannot_instaniate
     assert_raises(NoMethodError) do
       dw = DataHut::DataWarehouse.new  
     end
+  end
+
+  def test_bad_meta
+    dh = DataHut.connect("foo")
+    dh.store_meta(:bar, "good data")
     
-  #  assert_equal "OHAI!", @meme.i_can_has_cheezburger?
+    Marshal.expects(:dump).raises(RuntimeError.new("oh that was bad!"))
+    assert_raises(ArgumentError) do
+      dh.store_meta(:bar, "a bad thing")
+    end
+
+    Marshal.expects(:load).raises(RuntimeError.new("ouch!! again?"))
+    assert_raises(ArgumentError) do
+      dh.fetch_meta(:bar)
+    end
   end
 
 end
